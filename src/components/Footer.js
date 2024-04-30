648eff5b8c14aceb16425c7417ec6702bf71e78d
Henman2 <47754370+Henman2@users.noreply.github.com>
2024-04-30 15:21:25 +0000

    Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 const Footer = ()=>{
-
+    const currentYear = new Date().getFullYear();
+ return (
+     <footer>
+         <p>Developed by Henmanit<link href="www.henmanit.com"></link>copyright © {currentYear}</p>
+     </footer>
+ )
 }
 export default Footer;

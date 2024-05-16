886ed620585f43ecb7c0888e7f30cf10dedea3a6
Henman2 <47754370+Henman2@users.noreply.github.com>
2024-05-16 08:03:24 +0000

    Add React-Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./Pages/HomePage";
 import './styles/App.css';
 
 const App = ()=> {
-  return (
-      <div className="App">
-          <Header/>
-          <main>
-              <HomePage/>
-          </main>
-          <Footer/>
-      </div>
-  );
-}
+    return (
+        <div className="App">
+            <Header/>
+            <main>
+            <Routes>
+                <Route path='/' element={<HomePage/>} />
+            </Routes>
+                
+            </main>
+            <Footer/>
+        </div>
+    );
+    }
 export default App;
